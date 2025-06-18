9350a813164f20fbd174cdede1482fa7b986e989
khp9798 <jmn9798@naver.com>
2025-06-18 20:54:57 +0900

    ✨ feat: 사용자 보유 재료 entity 추가

--- Backend/src/main/java/hp/backend/domain/UserIngredient.java
+++ Backend/src/main/java/hp/backend/domain/UserIngredient.java
@@ -1,4 +1,35 @@
 package hp.backend.domain;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class UserIngredient {
+
+	@Id @GeneratedValue
+	private Long userIngredientId;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "ingredient_id")
+	private Ingredient ingredient;
+
+	@Column(name = "expiration_date")
+	private LocalDate expirationDate;
+
+	@Column(nullable = true)
+	private Float amount;
+
+	@Column(nullable = true, length = 20)
+	private String unit;
+
 }
