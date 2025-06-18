848365a847f3219140d43ed18a94c4dbed3c1f42
khp9798 <jmn9798@naver.com>
2025-06-18 20:49:34 +0900

    ✨ feat: 레시피에 들어가는 재료 entity 추가

--- Backend/src/main/java/hp/backend/domain/RecipeIngredient.java
+++ Backend/src/main/java/hp/backend/domain/RecipeIngredient.java
@@ -1,4 +1,23 @@
 package hp.backend.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class RecipeIngredient {
+
+	@Id @GeneratedValue
+	private Long recipeIngredientId;
+
+	@ManyToOne
+	@JoinColumn(name = "ingredient_id")
+	private Ingredient ingredient;
+
+	@ManyToOne
+	@JoinColumn(name = "recipe_id")
+	private Recipe recipe;
 }
