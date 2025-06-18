cb59c6d59b66e7032ea352602e48cdea083fc556
khp9798 <jmn9798@naver.com>
2025-06-18 20:38:12 +0900

    ✨ feat: bookmark entity 추가

--- Backend/src/main/java/hp/backend/domain/Bookmark.java
+++ Backend/src/main/java/hp/backend/domain/Bookmark.java
@@ -1,4 +1,24 @@
 package hp.backend.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Bookmark {
+
+	@Id @GeneratedValue
+	@Column(name = "id")
+	private Long bookmarkId;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "recipe_id")
+	private Recipe recipe;
 }
