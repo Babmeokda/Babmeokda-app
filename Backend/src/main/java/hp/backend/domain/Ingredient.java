d246cc7b6a64acbe6174db7a27210a58ec59ac09
khp9798 <jmn9798@naver.com>
2025-06-18 20:41:30 +0900

    ✨ feat: 재료 entity 추가

--- Backend/src/main/java/hp/backend/domain/Ingredient.java
+++ Backend/src/main/java/hp/backend/domain/Ingredient.java
@@ -1,4 +1,17 @@
 package hp.backend.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Ingredient {
+
+	@Id @GeneratedValue
+	@Column(name = "ingredient_id")
+	private Long ingredientId;
+
+	@Column(nullable = false, name = "ingredient_name", length = 255)
+	private String ingredientName;
 }
