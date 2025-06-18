7a258e5c24139161c03d5d4d033333373cfa1810
khp9798 <jmn9798@naver.com>
2025-06-18 20:49:58 +0900

    ✨ feat: 레시피 과정 entity 추가

--- Backend/src/main/java/hp/backend/domain/RecipeProgress.java
+++ Backend/src/main/java/hp/backend/domain/RecipeProgress.java
@@ -1,4 +1,26 @@
 package hp.backend.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class RecipeProgress {
+
+	@Id
+	@GeneratedValue
+	private Long recipeProgressId;
+
+	@ManyToOne
+	@JoinColumn(name = "recipe_id")
+	private Recipe recipe;
+
+	@Column(name = "step_no",nullable = false)
+	private int stepNumber;
+
+	@Column(name = "recipe_progress_description",columnDefinition = "TEXT" ,nullable = false)
+	private String description;
 }
