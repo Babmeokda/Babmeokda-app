9be5e7ac2092c15e1b43cd58681a494f95dff87a
khp9798 <jmn9798@naver.com>
2025-06-18 20:26:19 +0900

    ✨ feat: recipe entity 추가

--- Backend/src/main/java/hp/backend/domain/Recipe.java
+++ Backend/src/main/java/hp/backend/domain/Recipe.java
@@ -1,4 +1,22 @@
 package hp.backend.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Recipe {
+
+	@Id @GeneratedValue
+	@Column(name = "id")
+	private Long recipeId;
+
+	@Column(nullable = false, length = 255)
+	private String title;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String description;
+
+	private String imageUrl;
 }
