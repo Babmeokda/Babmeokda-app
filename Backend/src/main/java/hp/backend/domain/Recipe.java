5944ebcc8e5050c2da1fcd84d3092dec7b29835f
khp9798 <jmn9798@naver.com>
2025-06-18 20:44:26 +0900

    ♻️ refactor: 레시피 컬럼명 가독성 있게 변경(prefix recipe 추가)

--- Backend/src/main/java/hp/backend/domain/Recipe.java
+++ Backend/src/main/java/hp/backend/domain/Recipe.java
@@ -9,14 +9,12 @@ import jakarta.persistence.Id;
 public class Recipe {
 
 	@Id @GeneratedValue
-	@Column(name = "id")
+	@Column(name = "recipe_id")
 	private Long recipeId;
 
-	@Column(nullable = false, length = 255)
+	@Column(nullable = false, length = 255, name = "recipe_title")
 	private String title;
 
-	@Column(nullable = false, columnDefinition = "TEXT")
+	@Column(nullable = false, columnDefinition = "TEXT", name = "recipe_description")
 	private String description;
-
-	private String imageUrl;
 }
