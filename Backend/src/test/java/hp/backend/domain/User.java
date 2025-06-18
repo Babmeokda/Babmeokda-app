fe847d7f24d3f13e8354c3b25e01a2d1d622b848
khp9798 <jmn9798@naver.com>
2025-06-18 20:10:43 +0900

    ✨ feat: user entity 추가

---
+++ Backend/src/test/java/hp/backend/domain/User.java
@@ -0,0 +1,21 @@
+package hp.backend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private Long userId;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String nickname;
+}
